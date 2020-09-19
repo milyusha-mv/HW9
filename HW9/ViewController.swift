50c94eb8832ad0bd4042094651b15609641671b2
Mikhail M <milyusha-mv@ural.rt.ru>
2020-09-19 18:22:47 +0500

    Some work

--- HW9/ViewController.swift
+++ HW9/ViewController.swift
@@ -5,15 +5,103 @@
 //  Created by Михаил Милюша on 18.09.2020.
 //
 
-import UIKit
+import Spring
 
 class ViewController: UIViewController {
 
+    @IBOutlet var animationViewLable: UILabel!
+    @IBOutlet var startAnimationButton: UIButton!
+    @IBOutlet var animationView: SpringView!
+    
+    private let animationPresent = ["slideLeft",
+                             "slideRight",
+                             "slideDown",
+                             "slideUp",
+                             "squeezeLeft",
+                             "squeezeRight",
+                             "squeezeDown",
+                             "squeezeUp",
+                             "fadeIn",
+                             "fadeOut",
+                             "fadeOutIn",
+                             "fadeInLeft",
+                             "fadeInRight",
+                             "fadeInDown",
+                             "fadeInUp",
+                             "zoomIn",
+                             "zoomOut",
+                             "fall",
+                             "shake",
+                             "pop",
+                             "flipX",
+                             "flipY",
+                             "morph",
+                             "squeeze",
+                             "flash",
+                             "wobble",
+                             "swing"]
+    
+    private let animationCurve = ["easeIn",
+                                  "easeOut",
+                                  "easeInOut",
+                                  "linear",
+                                  "spring",
+                                  "easeInSine",
+                                  "easeOutSine",
+                                  "easeInOutSine",
+                                  "easeInQuad",
+                                  "easeOutQuad",
+                                  "easeInOutQuad",
+                                  "easeInCubic",
+                                  "easeOutCubic",
+                                  "easeInOutCubic",
+                                  "easeInQuart",
+                                  "easeOutQuart",
+                                  "easeInOutQuart",
+                                  "easeInQuint",
+                                  "easeOutQuint",
+                                  "easeInOutQuint",
+                                  "easeInExpo",
+                                  "easeOutExpo",
+                                  "easeInOutExpo",
+                                  "easeInCirc",
+                                  "easeOutCirc",
+                                  "easeInOutCirc",
+                                  "easeInBack",
+                                  "easeOutBack",
+                                  "easeInOutBack"]
+    
+    private var animationPresentElement = "slideLeft"
+    private var animationCurveElement = "easeIn"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        startAnimationButton.layer.cornerRadius = 10
+        animationView.layer.cornerRadius = 10
+        
+        animationViewLable.numberOfLines = 0
+        animationViewLable.isHidden = true
+        animationViewLable.textAlignment = .left
+        
+        
     }
 
-
+    @IBAction func startAnimationAction() {
+        animationView.animation = animationPresentElement
+        animationView.curve = animationCurveElement
+        
+        animationViewLable.text = """
+        animation: \(animationPresentElement)\n
+        curve:  \(animationCurveElement)
+        """
+        animationViewLable.isHidden = false
+        
+        animationView.animate()
+        
+        animationPresentElement = animationPresent.randomElement() ?? "slideLeft"
+        animationCurveElement = animationCurve.randomElement() ?? "easeIn"
+        
+        startAnimationButton.setTitle("Next \"\(animationPresentElement)\"", for: .normal)
+    }
 }
 
